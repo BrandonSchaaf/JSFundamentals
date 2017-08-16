7da08d25d208979f0ebc5c1e9aae3198d0dd593f
Brandon Schaaf <brandonschaaf@gmail.com>
2017-08-16 13:15:03 -0400

    Added js data types

---
+++ 0.01_dataTypes.js
@@ -0,0 +1,48 @@
+// Data Types:
+
+
+
+
+// STRINGS: A collection of characters 
+	
+	// (Enclose in double or single quotations)
+
+	"What a wonderful world.";
+	'Hi guys!';
+
+	// (Use one to protect the use of the other)
+
+	"It's a wonderful day."
+	'Jean-Luc Picard said "Make it so."'
+
+	// (If you need to use both, "escape" the job of the mark with a backslash)
+
+	'"Things can\'t get any better," he said.'
+
+	// (console.log can interact with a string with actions)
+
+	console.log('This is a way to print how many characters are in this string' .length )
+
+	console.log('This is a way to manipulate the string into all capital letters.' .toUpperCase())
+
+	// CHALLENGE: Create a string that has your name and use a function to make each character lower case:
+
+	console.log('BrAndoN bAILeY SchAaF' .toLowerCase())
+
+	console.log('this is a way to print a specific character from a string, capitalized.' [0] .toUpperCase());
+	
+	// (This prints each of the three characters, but with the first one capitalized, this is concatenation)
+
+	console.log('jay' [0] .toUpperCase() + 'jay'[1] + 'jay'[2]);
+
+	// CHALLENGE: Capitalize the 'm' in 'Home' and put the string back together using concatenation:
+
+	console.log('Home' [0] + 'Home' [1] + 'Home' [2] .toUpperCase() + 'Home' [3])
+
+// NUMBERS
+
+
+
+
+
+// BOOLEANS
